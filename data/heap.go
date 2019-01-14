7c19585005c656f4bd29bf35dac58d2439e31b03
Ilango R <ilangokul@gmail.com>
2019-01-14 20:03:58 +0530

    Expose Heapify and Swap

--- data/heap.go
+++ data/heap.go
@@ -1,59 +1,67 @@
 package data
 
 /*A Heap is implementation of heap data structure: https://en.wikipedia.org/wiki/Binary_heap
-This is a generic heap. Call BuildMax or BuildMin to build Max-Heap or Min-Heap. */
+This is a generic heap. Call BuildMax or BuildMin to build Max-Heap or Min-Heap.
+Heap provides direct access to all its elements. Any operation: Heapify or Build changes the order of elements. */
 type Heap []int
 
 func (h Heap) left(i int) int {
-	return (i+1)<<1 - 1
+	return i<<1 + 1
 }
 
 func (h Heap) right(i int) int {
-	return (i + 1) << 1
+	return i<<1 + 2
 }
 
-func (h Heap) swap(i, j int) {
+/*Swap swaps values between two indices*/
+func (h Heap) Swap(i, j int) {
 	h[i], h[j] = h[j], h[i]
 }
 
-func (h Heap) maxheapify(i int) {
+/*MaxHeapify "heapifies" the heap at given index.
+https://en.wikipedia.org/wiki/Binary_heap#Heap_operations
+This is for internal use, but exported for study and research. */
+func (h Heap) MaxHeapify(i int) {
 	if i >= len(h)/2 {
 		return
 	}
 	l, r := h.left(i), h.right(i)
 	if h[l] > h[i] && h[l] > h[r] {
-		h.swap(i, l)
-		h.maxheapify(l)
+		h.Swap(i, l)
+		h.MaxHeapify(l)
 	} else if h[r] > h[i] && h[r] > h[l] {
-		h.swap(i, r)
-		h.maxheapify(r)
+		h.Swap(i, r)
+		h.MaxHeapify(r)
 	}
 }
 
 /*BuildMax builds a Max-Heap from the given slice.*/
 func (h Heap) BuildMax() {
 	for i := len(h)/2 - 1; i >= 0; i-- {
-		h.maxheapify(i)
+		h.MaxHeapify(i)
 	}
 }
 
-func (h Heap) minheapify(i int) {
+/*MinHeapify "heapifies" the heap at given index.
+https://en.wikipedia.org/wiki/Binary_heap#Heap_operations
+This is for internal use, but exported for study and research. */
+func (h Heap) MinHeapify(i int) {
 	if i >= len(h)/2 {
 		return
 	}
 	l, r := h.left(i), h.right(i)
 	if h[l] < h[i] && h[l] < h[r] {
-		h.swap(i, l)
-		h.minheapify(l)
+		h.Swap(i, l)
+		h.MinHeapify(l)
 	} else if h[r] < h[i] && h[r] < h[l] {
-		h.swap(i, r)
-		h.minheapify(r)
+		h.Swap(i, r)
+		h.MinHeapify(r)
 	}
 }
 
 /*BuildMin builds a Min-Heap from the given slice.*/
 func (h Heap) BuildMin() {
 	for i := len(h)/2 - 1; i >= 0; i-- {
-		h.minheapify(i)
+		h.MinHeapify(i)
 	}
 }
