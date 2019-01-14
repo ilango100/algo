cc8af13da9972e49d6c885e5bec54e6db333bc9d
Ilango R <ilangokul@gmail.com>
2019-01-14 23:36:38 +0530

    Use direct arrays instead of struct

--- data/queue.go
+++ data/queue.go
@@ -1,20 +1,20 @@
 package data
 
-/*A Queue is a FIFO collection.*/
-type Queue struct {
-	arr []int
-}
+/*A Queue is a FIFO collection.
+You can use a prefilled []int to start with those elements.
+	Queue([]int{1,2,3}).Dequeue() //=>1*/
+type Queue []int
 
 /*Enqueue adds a new element in the queue.
 Returns the same Queue for chaining, i.e: q.Enqueue(1).Enqueue(2) */
 func (q *Queue) Enqueue(i int) *Queue {
-	q.arr = append(q.arr, i)
+	*q = append(*q, i)
 	return q
 }
 
 /*Dequeue extracts the next element from the queue*/
 func (q *Queue) Dequeue() int {
-	r := q.arr[0]
-	q.arr = q.arr[1:]
+	r := (*q)[0]
+	*q = (*q)[1:]
 	return r
 }
--- data/stack.go
+++ data/stack.go
@@ -1,21 +1,21 @@
 package data
 
-/*A Stack is a LIFO collection.*/
-type Stack struct {
-	arr []int
-}
+/*A Stack is a LIFO collection.
+You can use a prefilled []int to start with those elements.
+	Stack([]int{1,2,3}).Pop() //=>3*/
+type Stack []int
 
 /*Push pushes an element into the stack.
 Returns stack for chaining, i.e: s.Push(1).Push(1).*/
 func (s *Stack) Push(i int) *Stack {
-	s.arr = append(s.arr, i)
+	*s = append(*s, i)
 	return s
 }
 
 /*Pop pops the next element from the stack.*/
 func (s *Stack) Pop() int {
-	l := len(s.arr) - 1
-	r := s.arr[l]
-	s.arr = s.arr[:l]
+	l := len(*s) - 1
+	r := (*s)[l]
+	*s = (*s)[:l]
 	return r
 }
