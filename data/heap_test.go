94eb870949a9bf29881aadd3abcdeefe6f96a410
Ilango R <ilangokul@gmail.com>
2019-01-14 21:46:10 +0530

    Increase coverage in heap

--- data/heap_test.go
+++ data/heap_test.go
@@ -1,18 +1,16 @@
 package data
 
 import (
-	"fmt"
+	"math/rand"
 	"testing"
 )
 
 func TestMaxHeap(t *testing.T) {
-	h := Heap([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+	h := Heap(rand.Perm(1000))
 	h.BuildMax()
-	fmt.Println(h)
 }
 
 func TestMinHeap(t *testing.T) {
-	h := Heap([]int{7, 6, 5, 4, 3, 2, 1, 8, 9, 10, 11, 12})
+	h := Heap(rand.Perm(1000))
 	h.BuildMin()
-	fmt.Println(h)
 }
