33e5dbf83caac95563c94b987c694c2c8fd3fda3
Ilango R <ilangokul@gmail.com>
2019-01-14 20:51:35 +0530

    Heapify first tests for right child

--- data/heap.go
+++ data/heap.go
@@ -1,7 +1,8 @@
 package data
 
-/*A Heap is implementation of heap data structure: https://en.wikipedia.org/wiki/Binary_heap
+/*A Heap is implementation of heap data structure: https://en.wikipedia.org/wiki/Binary_heap.
 This is a generic heap. Call BuildMax or BuildMin to build Max-Heap or Min-Heap.
+
 Heap provides direct access to all its elements. Any operation: Heapify or Build changes the order of elements. */
 type Heap []int
 
@@ -9,8 +10,17 @@ func (h Heap) left(i int) int {
 	return i<<1 + 1
 }
 
-func (h Heap) right(i int) int {
-	return i<<1 + 2
+func (h Heap) right(i int) (int, bool) {
+	r := i<<1 + 2
+	return r, r < len(h)
+}
+
+func (h Heap) parent(i int) int {
+	return (i - 1) >> 1
+}
+
+func (h Heap) lastparent() int {
+	return len(h)>>1 - 1
 }
 
 /*Swap swaps values between two indices*/
@@ -22,22 +32,29 @@ func (h Heap) Swap(i, j int) {
 https://en.wikipedia.org/wiki/Binary_heap#Heap_operations
 This is for internal use, but exported for study and research. */
 func (h Heap) MaxHeapify(i int) {
-	if i >= len(h)/2 {
+	if i > h.lastparent() {
 		return
 	}
-	l, r := h.left(i), h.right(i)
-	if h[l] > h[i] && h[l] > h[r] {
-		h.Swap(i, l)
-		h.MaxHeapify(l)
-	} else if h[r] > h[i] && h[r] > h[l] {
-		h.Swap(i, r)
-		h.MaxHeapify(r)
+	l := h.left(i)
+	if r, ok := h.right(i); ok {
+		if h[l] > h[i] && h[l] > h[r] {
+			h.Swap(i, l)
+			h.MaxHeapify(l)
+		} else if h[r] > h[i] && h[r] > h[l] {
+			h.Swap(i, r)
+			h.MaxHeapify(r)
+		}
+	} else {
+		if h[l] > h[i] {
+			h.Swap(i, l)
+			h.MaxHeapify(l)
+		}
 	}
 }
 
 /*BuildMax builds a Max-Heap from the given slice.*/
 func (h Heap) BuildMax() {
-	for i := len(h)/2 - 1; i >= 0; i-- {
+	for i := h.lastparent(); i >= 0; i-- {
 		h.MaxHeapify(i)
 	}
 }
@@ -46,22 +63,29 @@ func (h Heap) BuildMax() {
 https://en.wikipedia.org/wiki/Binary_heap#Heap_operations
 This is for internal use, but exported for study and research. */
 func (h Heap) MinHeapify(i int) {
-	if i >= len(h)/2 {
+	if i > h.lastparent() {
 		return
 	}
-	l, r := h.left(i), h.right(i)
-	if h[l] < h[i] && h[l] < h[r] {
-		h.Swap(i, l)
-		h.MinHeapify(l)
-	} else if h[r] < h[i] && h[r] < h[l] {
-		h.Swap(i, r)
-		h.MinHeapify(r)
+	l := h.left(i)
+	if r, ok := h.right(i); ok {
+		if h[l] < h[i] && h[l] < h[r] {
+			h.Swap(i, l)
+			h.MaxHeapify(l)
+		} else if h[r] < h[i] && h[r] < h[l] {
+			h.Swap(i, r)
+			h.MaxHeapify(r)
+		}
+	} else {
+		if h[l] < h[i] {
+			h.Swap(i, l)
+			h.MaxHeapify(l)
+		}
 	}
 }
 
 /*BuildMin builds a Min-Heap from the given slice.*/
 func (h Heap) BuildMin() {
-	for i := len(h)/2 - 1; i >= 0; i-- {
+	for i := h.lastparent(); i >= 0; i-- {
 		h.MinHeapify(i)
 	}
 }
--- data/heap_test.go
+++ data/heap_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func TestMaxHeap(t *testing.T) {
-	h := Heap([]int{1, 2, 3, 4, 5, 6, 7})
+	h := Heap([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
 	h.BuildMax()
 	fmt.Println(h)
 }
 
 func TestMinHeap(t *testing.T) {
-	h := Heap([]int{7, 6, 5, 4, 3, 2, 1})
+	h := Heap([]int{7, 6, 5, 4, 3, 2, 1, 8, 9, 10, 11, 12})
 	h.BuildMin()
 	fmt.Println(h)
 }
